94d2a121ceb5aa3ab3a105678584a5b32bcb63b6
Mateus Belicio <mateusbelicio@hotmail.com>
2023-05-18 22:54:54 -0300

    Fix: Link on footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,14 +2,14 @@ import Logo from './Logo';
 
 export const Footer = () => {
   return (
-    <footer className='bg-black-400 text-white text-footer py-20 md:py-[4.875rem] '>
+    <footer className='text-footer bg-black-400 py-20 text-white md:py-[4.875rem] '>
       <div className='container gap-y-8'>
-        <div className='sm:col-span-2 mb-2'>
+        <div className='mb-2 sm:col-span-2'>
           <Logo />
         </div>
         <address className='not-italic sm:col-start-5 sm:col-end-13 md:col-start-4 md:col-end-8'>
           Marthwaite, Sedbergh <br /> Cumbria
-          <a href='tel:+00 44 123 4567' className='hover:underline focus-visible:underline block'>
+          <a href='tel:+00441234567' className='block hover:underline focus-visible:underline'>
             +00 44 123 4567
           </a>
         </address>
